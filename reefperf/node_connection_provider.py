d477e64b171b3f153645f2d54e6c69fd2ed8d974
Krzysztof Gromadzki <krzysztof.gromadzki@reef.pl>
2017-08-18 11:12:06 +0200

    Ident by 4 spaces

--- reefperf/node_connection_provider.py
+++ reefperf/node_connection_provider.py
@@ -5,44 +5,44 @@ from paramiko.client import MissingHostKeyPolicy
 from io import StringIO
 
 class NodeConnection(object):
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-	@abstractmethod
-	def exec_command(self, command_str):
-		pass
+    @abstractmethod
+    def exec_command(self, command_str):
+        pass
 
-	@abstractmethod
-	def scp_file(self, path_to_file):
-		pass
+    @abstractmethod
+    def scp_file(self, path_to_file):
+        pass
 
 class ParamikoConnection(NodeConnection):
-	KEY_SIZE = 4096
-	KEY_CLASSES = {
-		'rsa': RSAKey
-	}
+    KEY_SIZE = 4096
+    KEY_CLASSES = {
+        'rsa': RSAKey
+    }
 
-	def __init__(self, conn_cfg, ipv4, username):
-		priv_key_type = conn_cfg['keys']['keys_type']
-		priv_key_str = conn_cfg['keys']['priv_key_str']
-		priv_key_stream = StringIO(priv_key_str)
-		priv_key = self.KEY_CLASSES[priv_key_type].from_private_key(priv_key_stream)
-		self._client = SSHClient()
-		self._client.set_missing_host_key_policy(AutoAddPolicy())
-		self._client.connect(ipv4, username=username, pkey=priv_key)
+    def __init__(self, conn_cfg, ipv4, username):
+        priv_key_type = conn_cfg['keys']['keys_type']
+        priv_key_str = conn_cfg['keys']['priv_key_str']
+        priv_key_stream = StringIO(priv_key_str)
+        priv_key = self.KEY_CLASSES[priv_key_type].from_private_key(priv_key_stream)
+        self._client = SSHClient()
+        self._client.set_missing_host_key_policy(AutoAddPolicy())
+        self._client.connect(ipv4, username=username, pkey=priv_key)
 
-	def exec_command(self, command_str):
-		_, stdout_, _ =self._client.exec_command(command_str)
-		for line in stdout_.readlines():
-			print(line)
+    def exec_command(self, command_str):
+        _, stdout_, _ =self._client.exec_command(command_str)
+        for line in stdout_.readlines():
+            print(line)
 
-	def close(self):
-		self._client.close()
+    def close(self):
+        self._client.close()
 
 class NodeConnectionProvider(object):
-	CONNECTION_CLASSES = {
-		'paramiko': ParamikoConnection
-	}
+    CONNECTION_CLASSES = {
+        'paramiko': ParamikoConnection
+    }
 
-	@classmethod
-	def create_connection(cls, conn_cfg, ipv4, username):
-		return cls.CONNECTION_CLASSES[conn_cfg['connection_class']](conn_cfg, ipv4, username)	
+    @classmethod
+    def create_connection(cls, conn_cfg, ipv4, username):
+        return cls.CONNECTION_CLASSES[conn_cfg['connection_class']](conn_cfg, ipv4, username)   
--- scp_on_cloud_node.py
+++ scp_on_cloud_node.py
@@ -6,18 +6,18 @@ import paramiko
 from io import StringIO
 
 if __name__ == '__main__':
-	credentials = {
-		'api_token': 'token'
-	}
-	cloud_node_cfg = {
-		'node_name': 'node1',
-		'size': 'flex-2',
-		'image': 'ubuntu-16.04',
-		'connection': {
-			'connection_class': 'paramiko'
-		}
-	}
-	driver = LCCloudScaleDriver(credentials)
-	node = driver.create_node(cloud_node_cfg)
-	node.connection.exec_command('ls /')
-	node.destroy()
+    credentials = {
+        'api_token': 'token'
+    }
+    cloud_node_cfg = {
+        'node_name': 'node1',
+        'size': 'flex-2',
+        'image': 'ubuntu-16.04',
+        'connection': {
+            'connection_class': 'paramiko'
+        }
+    }
+    driver = LCCloudScaleDriver(credentials)
+    node = driver.create_node(cloud_node_cfg)
+    node.connection.exec_command('ls /')
+    node.destroy()
